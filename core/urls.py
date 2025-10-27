9753f5ebfd7f5f4977073c104e7125e316b9a822
Tobias Limburg <tobias.limburg@swisscom.com>
2025-10-27 08:26:05 +0100

    / redirect

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from core.views.application_list_view import ApplicationListView
 
 app_name = 'core'
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='core:application_list', permanent=False), name='home'),
     path('application/', ApplicationListView.as_view(), name='application_list'),
 ]
